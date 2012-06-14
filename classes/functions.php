efed85b768350d25cfcb961a9c0d2756aa538aeb
Sindre Svendby <sinsvend@gmail.com>
2012-06-14 10:17:33 +0200

    Done som work on retriving the users from dokus

--- classes/functions.php
+++ classes/functions.php
@@ -1,16 +1,15 @@
 <?php
 
+define("WORDPRESS_DOKUS_USER_FIELD" , "dokus_id");
+define("WORDPRESS_DOKUS_GROUP_FIELD",  "dokus_group_ids");
 
 function get_dokusWpUsers()
 {
-    $wordpress_dokus_user_field = dokus_id;
-    $wordpress_dokus_group_field = dokus_group_ids;
-
     $users = get_users();
     $dokusWpUsers = array();
     foreach( $users as  $user):
-         $dokus_user_id = get_user_meta($user, $wordpress_dokus_user_field);
-         $dokus_groups = get_user_meta($user, $wordpress_dokus_group_field);
+         $dokus_user_id = get_user_meta($user, WORDPRESS_DOKUS_USER_FIELD );
+         $dokus_groups = get_user_meta($user, WORDPRESS_DOKUS_GROUP_FIELD);
          $dokusWpUser = new DokusWPUser();
          $dokusWpUser->set_d_id($dokus_user_id);
          $dokusWpUser->set_d_groups($dokus_groups);
@@ -24,8 +23,41 @@ function get_dokusWpUsers()
 }
 
 
+function get_dokus_users_not_in_wp() {
+    $dokus_users = get_dokus_user(null);
+    $dokus_ids = get_dokus_ids_in_wordpress();
+    $dokus_users_not_in_wp = array();
+    foreach($dokus_users as $dokus_user):
+        if(!in_array($dokus_user->ID, $dokus_ids)):
+            $dokus_users_not_in_wp[] = $dokus_user->ID;
+        endif;
+    endforeach;
+    return $dokus_users_not_in_wp;
+}
+
+function get_dokus_ids_in_wordpress() {
+    $users = get_users();
+    $ids = array();
+    foreach($users as $user):
+        $id = get_user_meta($user->ID, WORDPRESS_DOKUS_USER_FIELD);
+        if(!empty($id)):
+            $ids[] = $id;
+        endif;
+    endforeach;
+    return $ids;
+}
+
+/**
+ * Returns a dokus user or a list of dokus users.
+ * @param $id
+ * @return a Dokus User, or a list of Dokus Users if $id is empty.
+ *
+ */
 function get_dokus_user($id) {
 
     $dokusCustomersResource = new DokusCustomersResource(getDokusService());
+    if (empty($id)) {
+        return $dokusCustomersResource->all();
+    }
     return $dokusCustomersResource->get($id);
 }
--- pages/default.php
+++ pages/default.php
@@ -4,6 +4,7 @@
  * License under common sense and respect.
  */
 
+print_array(get_dokus_users_wp_ids());
 $dwp_users = get_dokusWpUsers();
 
 echo "<table>";
@@ -11,14 +12,23 @@ echo "<thead>";
 echo "<td>Wordpress User</td><td>Dokus User</td><td>Groups</td><td>Remove</td><td>Create</td>";
 echo "</thead>";
 
-foreach($dwp_users as $dwp_user):
+foreach ($dwp_users as $dwp_user):
     echo "<tr>";
-    echo "<td>". $dwp_user->get_w_name() ."</td>";
-    echo "<td>". $dwp_user->get_d_name() ."</td>";
-    echo "<td>". $dwp_user->get_d_groups() ."</td>";
-    echo "<td><a href='/options-general.php?page=dokus&remove_mapping_for_dokus_user_for_user='".$dwp_user->get_w_id().">Remove Mapping</a></td>";
-    echo "<td><a href='/options-general.php?page=dokus&create_dokus_user_based_on_user='".$dwp_user->get_w_id().">Create new dokus user based on this</a></td>";
-    echo "</tr>";
+    echo "<td>" . $dwp_user->get_w_name() . "</td>";
+    if ($dwp_user->get_d_id()):
+        echo "<td>" . $dwp_user->get_d_name() . "</td>";
+        echo "<td>";
+        foreach ($dwp_user->get_d_groups() as $group):
+            echo $group;
+        endforeach;
+        echo "</td>";
+        echo "<td><a href='/options-general.php?page=dokus&remove_mapping_for_dokus_user_for_user=" . $dwp_user->get_w_id() . "'>Remove Mapping</a></td>";
+        echo "<td></td>";
+    else:
+        echo "<td></td><td></td><td></td>";
+        echo "<td><a href='/options-general.php?page=dokus&create_dokus_user_based_on_user=" . $dwp_user->get_w_id() . "'>Create new dokus user based on " . $dwp_user->get_w_name() . "</a></td>";
+        echo "</tr>";
+    endif;
 endforeach;
 
 echo "</table>";
